41445bbf51dde94836c201be42c82f3fa47e0b3d
DiegoHZ5 <safairus@gmail.com>
2021-10-26 23:17:34 -0500

    Unit Test Implemented
    Unit Test Implemented
    
    Co-Authored-By: Patrick2OL <63985270+Patrick2OL@users.noreply.github.com>

--- src/test/java/com/edu/upc/ilanguageexternaltool/ILanguageExternalToolApplicationTests.java
+++ src/test/java/com/edu/upc/ilanguageexternaltool/ILanguageExternalToolApplicationTests.java
@@ -1,39 +1,63 @@
 package com.edu.upc.ilanguageexternaltool;
 
+import com.edu.upc.ilanguageexternaltool.command.application.services.ExternalToolApplicationService;
 import com.edu.upc.ilanguageexternaltool.command.domain.ExternalTool;
+import com.edu.upc.ilanguageexternaltool.command.infra.ExternalToolName;
+import com.edu.upc.ilanguageexternaltool.command.infra.ExternalToolNameRepository;
+import contracts.events.ExternalToolRegistered;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.Instant;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ILanguageExternalToolApplicationTests {
+    @MockBean
+    private ExternalToolNameRepository externalToolNameRepository;
 
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//    @Test
-//    @DisplayName("When getSessionByTopic With Valid topic Then Returns Session")
-//    public void whenGetSessionByTopicAtWithValidTopicAtThenReturnsSession() {
-//        // Arrange
-//        String topic = "active";
-//        ExternalTool externalTool = new ExternalTool();
-//        externalTool.setName("Test");
-//        externalTool.setDescription("Test");
-//        externalTool.setDescription("Test");
-//
-////        when(sessionRepository.findByTopic(topic))
-////                .thenReturn(Optional.of(session));
-////
-////        // Act
-////        Session foundSession = sessionService.getSessionByTopic(topic);
-////
-////        // Assert
-////        assertThat(foundSession.getTopic()).isEqualTo(topic);
-//    }
+    private ExternalToolApplicationService externalToolApplicationService;
+
+    @BeforeEach()
+    public void SetUp(){
+        //fixture = new AggregateTestFixture<ExternalTool>(ExternalTool.class);
+    }
+
+    @Test
+    @DisplayName("Get external tool by name with valid name then return true")
+    public void whenGetExternalToolByNameWithValidNameThenReturnsSubscription(){
+        //Arrange
+        String name = "FullPass";
+        ExternalToolName externalToolName  = new ExternalToolName();
+        externalToolName.setName(name);
+        externalToolName.setExternalToolId("HDUD5FSDFDSFGJ");
+        when(externalToolNameRepository.findByName(name)).thenReturn(Optional.of(externalToolName));
+        Optional<ExternalToolName> foundExternalTool = externalToolNameRepository.findByName(name);
+        AssertionsForClassTypes.assertThat(foundExternalTool.get().getName()).isEqualTo(externalToolName.getName());
+    }
+
+
+    @Test
+    @DisplayName("Get external tool by name with valid name then return true")
+    public void ssd(){
+        //https://docs.axoniq.io/reference-guide/v/3.3/part-ii-domain-logic/testing
+        /*fixture.given()
+                .when(new RegisterSubscription("sddsadsadsad","sadasd",5,100))
+                .expectSuccessfulHandlerExecution()
+                .expectReturnValue(new RegisterSubscriptionRes("sddsadsadsad","sadasd",5,100));
+               // .expectNoEvents(new SubscriptionRegistered("sddsadsadsad","sadasd", 5,100, Instant.now()));*/
+        apply(new ExternalToolRegistered("sddsadsadsad","sadasd","sd","100", Instant.now()));
+        assertEquals(1, AggregateLifecycle.isLive());
+
+    }
 }
